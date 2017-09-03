058ef80e49f3223ee81c33279c61c45eee2d6740
Xinghai Sun <sunxinghai1216@gmail.com>
2017-09-03 17:51:25 +0800

    Add broadcasting support (e.g. matrix-vector) for cos sim operator.

--- paddle/operators/cos_sim_op.h
+++ paddle/operators/cos_sim_op.h
@@ -28,30 +28,38 @@ template <typename Place, typename T>
 class CosSimKernel : public framework::OpKernel {
  public:
   void Compute(const framework::ExecutionContext& context) const override {
+    // get Tensor
     auto* x = context.Input<Tensor>("X");
     auto* y = context.Input<Tensor>("Y");
     auto* z = context.Output<Tensor>("Out");
     auto* x_norm = context.Output<Tensor>("XNorm");
     auto* y_norm = context.Output<Tensor>("YNorm");
-
     z->mutable_data<T>(context.GetPlace());
     x_norm->mutable_data<T>(context.GetPlace());
     y_norm->mutable_data<T>(context.GetPlace());
 
-    auto dims = x->dims();
-    int size = static_cast<int>(framework::product(dims));
-    auto new_dims = framework::make_ddim({dims[0], size / dims[0]});
-    auto X = EigenMatrix<T>::From(*x, new_dims);
-    auto Y = EigenMatrix<T>::From(*y, new_dims);
+    // convert Tensor to Eigen Tensor
+    int rows_x = x->dims()[0];
+    int rows_y = y->dims()[0];
+    int cols = framework::product(x->dims()) / rows_x;
+    auto X = EigenMatrix<T>::From(*x, framework::make_ddim({rows_x, cols}));
+    auto Y = EigenMatrix<T>::From(*y, framework::make_ddim({rows_y, cols}));
     auto Z = EigenMatrix<T>::From(*z);
     auto XNorm = EigenMatrix<T>::From(*x_norm);
     auto YNorm = EigenMatrix<T>::From(*y_norm);
 
+    // compute
     auto place = context.GetEigenDevice<Place>();
-    auto XY = (X * Y).sum(Eigen::array<int, 1>({1}));
     XNorm.device(place) = (X * X).sum(Eigen::array<int, 1>({1})).sqrt();
     YNorm.device(place) = (Y * Y).sum(Eigen::array<int, 1>({1})).sqrt();
-    Z.device(place) = XY / XNorm / YNorm;
+    if (rows_x == rows_y) {
+      auto XY = (X * Y).sum(Eigen::array<int, 1>({1}));
+      Z.device(place) = XY / XNorm / YNorm;
+    } else {
+      Eigen::DSizes<int, 2> bcast(rows_x, 1);
+      auto XY = (X * Y.broadcast(bcast)).sum(Eigen::array<int, 1>({1}));
+      Z.device(place) = XY / XNorm / YNorm.broadcast(bcast);
+    }
   }
 };
 
@@ -59,6 +67,7 @@ template <typename Place, typename T>
 class CosSimGradKernel : public framework::OpKernel {
  public:
   void Compute(const framework::ExecutionContext& context) const override {
+    // get Tensor
     auto* x = context.Input<Tensor>("X");
     auto* y = context.Input<Tensor>("Y");
     auto* z = context.Input<Tensor>("Out");
@@ -67,33 +76,50 @@ class CosSimGradKernel : public framework::OpKernel {
     auto* grad_x = context.Output<Tensor>(framework::GradVarName("X"));
     auto* grad_y = context.Output<Tensor>(framework::GradVarName("Y"));
     auto* grad_z = context.Input<Tensor>(framework::GradVarName("Out"));
-
     grad_x->mutable_data<T>(context.GetPlace());
     grad_y->mutable_data<T>(context.GetPlace());
 
-    auto dims = x->dims();
-    int size = static_cast<int>(framework::product(dims));
-    auto new_dims = framework::make_ddim({dims[0], size / dims[0]});
-    auto X = EigenMatrix<T>::From(*x, new_dims);
-    auto Y = EigenMatrix<T>::From(*y, new_dims);
+    // convert Tensor to Eigen Tensor
+    int rows_x = x->dims()[0];
+    int rows_y = y->dims()[0];
+    int cols = framework::product(x->dims()) / rows_x;
+    auto X = EigenMatrix<T>::From(*x, framework::make_ddim({rows_x, cols}));
+    auto Y = EigenMatrix<T>::From(*y, framework::make_ddim({rows_y, cols}));
     auto Z = EigenMatrix<T>::From(*z);
     auto X_norm = EigenMatrix<T>::From(*x_norm);
     auto Y_norm = EigenMatrix<T>::From(*y_norm);
-    auto dX = EigenMatrix<T>::From(*grad_x, new_dims);
-    auto dY = EigenMatrix<T>::From(*grad_y, new_dims);
+    auto dX =
+        EigenMatrix<T>::From(*grad_x, framework::make_ddim({rows_x, cols}));
+    auto dY =
+        EigenMatrix<T>::From(*grad_y, framework::make_ddim({rows_y, cols}));
     auto dZ = EigenMatrix<T>::From(*grad_z);
 
-    Eigen::DSizes<int, 2> bcast(1, new_dims[1]);
+    // compute gradident
+    Eigen::DSizes<int, 2> bcast(1, cols);
     auto Z_bcast = Z.broadcast(bcast);
     auto dZ_bcast = dZ.broadcast(bcast);
-    auto place = context.GetEigenDevice<Place>();
     auto X_snorm_bcast = X_norm.square().eval().broadcast(bcast);
-    auto Y_snorm_bcast = Y_norm.square().eval().broadcast(bcast);
-    auto norm_prod_bcast = (X_norm * Y_norm).eval().broadcast(bcast);
-    dX.device(place) =
-        dZ_bcast * (Y / norm_prod_bcast - Z_bcast * X / X_snorm_bcast);
-    dY.device(place) =
-        dZ_bcast * (X / norm_prod_bcast - Z_bcast * Y / Y_snorm_bcast);
+    auto place = context.GetEigenDevice<Place>();
+    if (rows_x == rows_y) {
+      auto Y_snorm_bcast = Y_norm.square().eval().broadcast(bcast);
+      auto norm_prod_bcast = (X_norm * Y_norm).eval().broadcast(bcast);
+      dX.device(place) =
+          dZ_bcast * (Y / norm_prod_bcast - Z_bcast * X / X_snorm_bcast);
+      dY.device(place) =
+          dZ_bcast * (X / norm_prod_bcast - Z_bcast * Y / Y_snorm_bcast);
+    } else {
+      Eigen::DSizes<int, 2> bcast_row(rows_x, 1);
+      auto Y_snorm_bcast =
+          Y_norm.square().eval().broadcast(bcast_row).eval().broadcast(bcast);
+      auto norm_prod_bcast =
+          (X_norm * Y_norm.broadcast(bcast_row)).eval().broadcast(bcast);
+      dX.device(place) = dZ_bcast * (Y.broadcast(bcast_row) / norm_prod_bcast -
+                                     Z_bcast * X / X_snorm_bcast);
+      dY.device(place) =
+          (dZ_bcast * (X / norm_prod_bcast -
+                       Z_bcast * Y.broadcast(bcast_row) / Y_snorm_bcast))
+              .sum(Eigen::array<int, 1>({0}));
+    }
   }
 };
 
--- python/paddle/v2/framework/tests/test_cos_sim_op.py
+++ python/paddle/v2/framework/tests/test_cos_sim_op.py
@@ -4,7 +4,7 @@ from gradient_checker import GradientChecker, create_op
 from op_test_util import OpTestMeta
 
 
-class TestCosSimOp(unittest.TestCase):
+class TestCosSimOp2d(unittest.TestCase):
     __metaclass__ = OpTestMeta
 
     def setUp(self):
@@ -24,12 +24,62 @@ class TestCosSimOp(unittest.TestCase):
         }
 
 
-class CosSimGradOpTest(GradientChecker):
+class TestCosSimOp2dBcast(unittest.TestCase):
+    __metaclass__ = OpTestMeta
+
+    def setUp(self):
+        self.type = "cos_sim"
+        self.inputs = {
+            'X': np.random.random((32, 64)).astype("float32"),
+            'Y': np.random.random((1, 64)).astype("float32")
+        }
+        expect_x_norm = np.linalg.norm(self.inputs['X'], axis=1)
+        expect_y_norm = np.linalg.norm(self.inputs['Y'], axis=1)
+        expect_out = (self.inputs['X'] * self.inputs['Y']).sum(axis=1) / \
+            expect_x_norm / expect_y_norm
+        self.outputs = {
+            'XNorm': np.expand_dims(expect_x_norm, 1),
+            'YNorm': np.expand_dims(expect_y_norm, 1),
+            'Out': np.expand_dims(expect_out, 1)
+        }
+
+
+class TestCosSimOp3dBcast(unittest.TestCase):
+    __metaclass__ = OpTestMeta
+
+    def setUp(self):
+        self.type = "cos_sim"
+        self.inputs = {
+            'X': np.random.random((32, 64, 10)).astype("float32"),
+            'Y': np.random.random((1, 64, 10)).astype("float32")
+        }
+        expect_x_norm = np.linalg.norm(self.inputs['X'], axis=(1, 2))
+        expect_y_norm = np.linalg.norm(self.inputs['Y'], axis=(1, 2))
+        expect_out = (self.inputs['X'] * self.inputs['Y']).sum(axis=(1, 2)) / \
+            expect_x_norm / expect_y_norm
+        self.outputs = {
+            'XNorm': np.expand_dims(expect_x_norm, 1),
+            'YNorm': np.expand_dims(expect_y_norm, 1),
+            'Out': np.expand_dims(expect_out, 1)
+        }
+
+
+class TestCosSimGradOp(GradientChecker):
     def test_cos_sim_2d(self):
         op = create_op("cos_sim")
         inputs = {
-            'X': np.random.random((10, 5)).astype("float32"),
-            'Y': np.random.random((10, 5)).astype("float32")
+            'X': np.random.random((6, 5)).astype("float32"),
+            'Y': np.random.random((6, 5)).astype("float32")
+        }
+        self.compare_grad(op, inputs)
+        self.check_grad(
+            op, inputs, set(["X", "Y"]), "Out", max_relative_error=0.05)
+
+    def test_cos_sim_2d_bcast(self):
+        op = create_op("cos_sim")
+        inputs = {
+            'X': np.random.random((6, 5)).astype("float32"),
+            'Y': np.random.random((1, 5)).astype("float32")
         }
         self.compare_grad(op, inputs)
         self.check_grad(
@@ -38,8 +88,18 @@ class CosSimGradOpTest(GradientChecker):
     def test_cos_sim_3d(self):
         op = create_op("cos_sim")
         inputs = {
-            'X': np.random.random((10, 5, 2)).astype("float32"),
-            'Y': np.random.random((10, 5, 2)).astype("float32")
+            'X': np.random.random((6, 5, 2)).astype("float32"),
+            'Y': np.random.random((6, 5, 2)).astype("float32")
+        }
+        self.compare_grad(op, inputs)
+        self.check_grad(
+            op, inputs, set(["X", "Y"]), "Out", max_relative_error=0.05)
+
+    def test_cos_sim_3d_bcast(self):
+        op = create_op("cos_sim")
+        inputs = {
+            'X': np.random.random((6, 5, 2)).astype("float32"),
+            'Y': np.random.random((1, 5, 2)).astype("float32")
         }
         self.compare_grad(op, inputs)
         self.check_grad(
